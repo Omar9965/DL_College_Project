61734e49a3c526cb844fdbbc99d03a4be11e6852
Mohamed Ahmed Abdel Samie <112393650+m7md158@users.noreply.github.com>
2024-12-27 17:57:20 +0200

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -8,10 +8,10 @@ gender_model = tf.keras.models.load_model('gender_model.h5')
 
 def prepare_image(image):
     img = Image.open(image)
-    img = img.convert("RGB")  
+    img = img.convert("L")  
     img = img.resize((128, 128))  
     img = np.array(img)
-    img = img.reshape((1, 128, 128, 3))  
+    img = img.reshape((1, 128, 128, 1))  
     img = img / 255.0 
     return img
 
